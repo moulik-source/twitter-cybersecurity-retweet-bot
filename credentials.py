ade4b5c06b87369960db9569ae0fafb5828d2dc7
heyheykids <62848840+moulik-source@users.noreply.github.com>
2021-12-29 16:31:27 +0530

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,8 @@
+# This is just a sample file. You need to
+# edit this file. You need to get these
+# details from your Twitter app settings.
+
+consumer_key = 'ZB1zmKjyhkiL3K4rrrdfrpPIl'
+consumer_secret = '8UFDLZBo2NTPqhNpMI3tC72hvJQXhiR8sCHvgpJBnOTT3AT2Ct'
+access_token = '1364624646692249600-SjSDdPxXJ4cbs7KXuUPCSfYZbqfZFN'
+access_token_secret = 'lJbE2cSJnZ8dAhH1qQbVQdOfYk0Ao8j45ObLkfvKIoRCI'
