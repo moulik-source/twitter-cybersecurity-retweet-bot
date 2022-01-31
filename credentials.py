bc7b5af8aa0c69cdd26c96ae55f56ef491c12407
heyheykids <62848840+moulik-source@users.noreply.github.com>
2022-01-31 13:31:31 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -2,7 +2,7 @@
 # edit this file. You need to get these
 # details from your Twitter app settings.
 
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
+consumer_key = 'GUVzL5JLGblHzhWb2OJunAa6t'
+consumer_secret = 'SUCz8gNIoyHA6vuVPhQIlrAU28Rn42h5TCC0CdZGFtDwyONIto'
+access_token = '1364624646692249600-FkQEb7zka48m72zhe5TXQ60grsJQ3H'
+access_token_secret = '8y6Z0tdxy8X5lRkrLCW8IDPC06YNA7CQeL8xgoy74H3Qu'
